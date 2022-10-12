b0666d0c247a7d8f860ec228553292eb485127f1
Rakib5627 <mdrakib.5627@gmail.com>
2022-10-12 22:22:37 +0600

    navbar

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark py-5 mt-5'>
+            <p className='text-white py-5'><small>@copyright 2022</small>  <small>Rakibul Islam</small></p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
